56ba740a59d1f42cfa692790a67d4e8db64e5ceb
Thomas Nabord <thomas.nabord@prestashop.com>
2015-05-29 12:14:24 +0200

    Update version (v1.0.2)

--- nimblepayment.php
+++ nimblepayment.php
@@ -36,7 +36,7 @@ class NimblePayment extends PaymentModule
 	{
 		$this->name = 'nimblepayment';
 		$this->tab = 'payments_gateways';
-		$this->version = '1.0.1';
+		$this->version = '1.0.2';
 		$this->author = 'BBVA';
 
 		$this->bootstrap = true;
